ba11726e87084f6ab227c59a1978dcc1769f05ac
LagTheSystem <lagthesystem@duck.com>
2025-03-17 18:56:49 +0000

    Making ranking more concise

--- revised.py
+++ revised.py
@@ -3,12 +3,25 @@ import json
 with open('filtered_country_data.json', 'r') as file:
     data = json.load(file)
 
-data_types = ["tfr_score", "imr", "gii", "education", "life_expectancy", "gnipc", "rni", "hdi", "pop_online", "prison_pop", "co2_pc", "literacy_rate"]
+data_types = [
+    ("tfr_score", False),
+    ("imr", True),
+    ("gii", True),
+    ("education", False),
+    ("life_expectancy", False),
+    ("gnipc", False),
+    ("rni", True),
+    ("hdi", False),
+    ("pop_online", False),
+    ("prison_pop", True),
+    ("co2_pc", True),
+    ("literacy_rate", False),
+]
 
 ranks = []
 
-def rank(dic, key, reversed):
-    dic = sorted(dic, key = lambda d: d[key], reverse=reversed)
+def rank(dic, key, isReversed):
+    dic = sorted(dic, key = lambda d: d[key], reverse=isReversed)
     for i, entry in enumerate(dic):
         entry[key] = i
 
@@ -22,7 +35,7 @@ def calculateWins():
         entry["wins"] = 0
         for j, _ in enumerate(ranks):
             if j != i:
-                cat_wins = sum(1 for k in data_types if isBetter(ranks, k, i, j))
+                cat_wins = sum(1 for k, _ in data_types if isBetter(ranks, k, i, j))
                 if cat_wins >= 6:
                     wins += 1
         entry["wins"] = wins
@@ -45,19 +58,8 @@ for entry in data:
         "literacy_rate": entry["literacy_rate"]
     })
 
-# Stats that are better closer to zero must be reversed
-rank(ranks, "tfr_score", False)
-rank(ranks, "imr", True)
-rank(ranks, "gii", True)
-rank(ranks, "education", False)
-rank(ranks, "life_expectancy", False)
-rank(ranks, "gnipc", False)
-rank(ranks, "rni", True)
-rank(ranks, "hdi", False)
-rank(ranks, "pop_online", False)
-rank(ranks, "prison_pop", True)
-rank(ranks, "co2_pc", True)
-rank(ranks, "literacy_rate", False)
+for type, isReversed in data_types:
+    rank(ranks, type, isReversed)
 
 calculateWins()
 
