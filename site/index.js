8cf143a7fc5858f93c36843cfb4680e3a2f97fb5
LagTheSystem <lagthesystem@duck.com>
2025-03-19 14:23:59 +0000

    Reverting some breaking changes

--- site/index.js
+++ site/index.js
@@ -1,15 +1,9 @@
 import data from "./country_data.js"
 
 const select = document.getElementById("countries")
-const countryKeys = Object.keys(data[findIndex()]);
 
-/**
- * Function to get the index of the searched value
- * @param search The value to search for
- * @returns The index of the searched value
- */
-function findIndex(search) {
-    return data.findIndex(obj => obj.country === search);
+function findIndex() {
+    return data.findIndex(obj => obj.country === select.value);
 }
 
 data.forEach((c) => {
@@ -19,10 +13,12 @@ data.forEach((c) => {
     select.appendChild(o);
 })
 
+const countryKeys = Object.keys(data[findIndex()]);
+
 function selection() {
     for (let i = 0; i < countryKeys.length; i++) {
         const item = document.getElementById(countryKeys[i]);
-        item.innerHTML = data[findIndex(select.value)][countryKeys[i]];
+        item.innerHTML = data[findIndex()][countryKeys[i]];
     }
 }
 window.selection = selection;
